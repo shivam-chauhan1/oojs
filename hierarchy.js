9c5c135e5f6c88457219620a53f7fe3dcd43509f
Shivam Chauhan <shivam.chauhan1@tothenew.com>
2025-02-11 14:44:23 +0530

    hierarchy

--- hierarchy.js
+++ hierarchy.js
@@ -1,35 +1,35 @@
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-
-  greet() {
-    console.log(`Hello, my name is ${this.name}`);
-  }
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
 }
 
-class Employee extends Person {
-  constructor(name, age, company) {
-    super(name, age);
-    this.company = company;
-  }
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}`);
+};
 
-  work() {
-    console.log(`${this.name} works at ${this.company}`);
-  }
+function Employee(name, age, company) {
+  Person.call(this, name, age);
+  this.company = company;
 }
 
-class Developer extends Employee {
-  constructor(name, age, company, tech) {
-    super(name, age, company);
-    this.tech = tech;
-  }
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 
-  code() {
-    console.log(`${this.name} is coding in ${this.tech}`);
-  }
+Employee.prototype.work = function () {
+  console.log(`${this.name} works at ${this.company}`);
+};
+
+function Developer(name, age, company, tech) {
+  Employee.call(this, name, age, company);
+  this.tech = tech;
 }
+
+Developer.prototype = Object.create(Employee.prototype);
+Developer.prototype.constructor = Developer;
+
+Developer.prototype.code = function () {
+  console.log(`${this.name} is coding in ${this.tech}`);
+};
 
 const dev = new Developer("Shivam", 21, "ToTheNew", "JavaScript");
 dev.greet();
